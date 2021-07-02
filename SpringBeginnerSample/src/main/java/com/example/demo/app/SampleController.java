dc0eb82a0ad8f3c56f5fbd4c165d9a1ef71d8d9f
krystalmm <taeyeon_aina@live.jp>
2021-07-02 23:25:07 +0900

    [add] sampleController and testpage

--- SpringBeginnerSample/src/main/java/com/example/demo/app/SampleController.java
+++ SpringBeginnerSample/src/main/java/com/example/demo/app/SampleController.java
@@ -1,5 +1,17 @@
 package com.example.demo.app;
 
-public class SampleController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
+@RequestMapping("/sample")
+public class SampleController {
+	
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public String test(Model model) {
+		model.addAttribute("title", "Inqurity Form");
+		return "test";
+	}
 }

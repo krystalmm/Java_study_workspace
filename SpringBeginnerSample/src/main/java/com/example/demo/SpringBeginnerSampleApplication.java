cc4bc0fc13b143742507e468b751ef99a03a4b50
krystalmm <taeyeon_aina@live.jp>
2021-07-02 22:41:35 +0900

    [Start] lesson4

---
+++ SpringBeginnerSample/src/main/java/com/example/demo/SpringBeginnerSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeginnerSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBeginnerSampleApplication.class, args);
+	}
+
+}

b33409c53fcb146e39378b24aadefc10cb7e7c46
krystalmm <taeyeon_aina@live.jp>
2021-07-03 11:03:18 +0900

    [Finish] lesson4

--- SpringBeginnerSample/src/main/java/com/example/demo/app/SampleController.java
+++ SpringBeginnerSample/src/main/java/com/example/demo/app/SampleController.java
@@ -1,5 +1,9 @@
 package com.example.demo.app;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +13,29 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/sample")
 public class SampleController {
 	
+	// JdbcTemplateはjdbcでDBを操作するためのクラス！
+	private final JdbcTemplate jdbcTemplate;
+	
+	// jdbcTemplateはnewしないで、Dependency Injection（インスタンス化されたオブジェクトを自動で渡してくれる機能）を使う！
+	@Autowired
+	public SampleController(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+	
 	@RequestMapping(value = "/test", method = RequestMethod.GET)
 	public String test(Model model) {
+		
+		// DB操作の処理
+		String sql = "SELECT id, name, email"
+				+ " FROM inquiry WHERE id = 1";
+		// Stringはカラム名、Objectはカラムの値の型がstringやintと分かれてくるのでそうしている！
+		// queryForMapメソッドでデータ取得（引数に上記のsql文を指定！）
+		Map<String, Object> map = jdbcTemplate.queryForMap(sql);
+		// 取得したデータをHTML上で表示するためmodelに追加！
+		// map.get()で返ってくる型はObjectだが、Stringに変換される！
+		model.addAttribute("name", map.get("name"));
+		model.addAttribute("email", map.get("email"));
+		
 		model.addAttribute("title", "Inqurity Form");
 		return "test";
 	}

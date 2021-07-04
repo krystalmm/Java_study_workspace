3016be6deff9c6da12ae04dc54b1a29a3125229a
krystalmm <taeyeon_aina@live.jp>
2021-07-04 12:00:06 +0900

    [add] complete and flash scope

--- SpringBeginnerSample/src/main/java/com/example/demo/app/inquiry/InquiryController.java
+++ SpringBeginnerSample/src/main/java/com/example/demo/app/inquiry/InquiryController.java
@@ -4,15 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/inquiry")
 public class InquiryController {
 	
 	@RequestMapping(value = "/form", method = RequestMethod.GET)
-	public String form(InquiryForm inquiryForm, Model model) {
+	// フラッシュスコープを受け取るため@ModelAttributeを引数に指定！
+	public String form(InquiryForm inquiryForm, Model model, @ModelAttribute("complete") String complete) {
 		model.addAttribute("title", "Inquiry Form");
 		return "inquiry/form";
 	}
@@ -32,5 +35,18 @@ public class InquiryController {
 		
 		model.addAttribute("title", "Confirm Page");
 		return "inquiry/confirm";
+	}
+	
+	// 確認フォームの内容は裏からいじることもできるので、validationを入れておく！
+	// RedirectAttributesはフラッシュスコープ（セッションを用いてできている）を使うためのもの！リダイレクトするとmodel.addAttributeが使えないので、これを使う！
+	@RequestMapping(value = "/complete", method = RequestMethod.POST)
+	public String complete(@Validated InquiryForm inquiryForm, BindingResult result, Model model, RedirectAttributes redirectAttributes) {
+		if(result.hasErrors()) {
+			model.addAttribute("title", "InquiryForm");
+			return "inquiry/form";
+		}
+		// フラッシュスコープを使って一時的なメッセージを表示させる！
+		redirectAttributes.addFlashAttribute("complete", "Registered!");
+		return "redirect:/inquiry/form";
 	}
 }
